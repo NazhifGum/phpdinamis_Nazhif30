19434151bcd883af8c910e799bc2b094e4f75975
unknown <Nazhiframadhani72@gmail.com>
2021-11-19 08:09:00 +0700

    ini commit pertama kamu

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+
+          
+          <!-- ini adalah navbar -->
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+              <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Nazhif Alwi Ramadhani</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Alamat</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Hobi</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                cita cita
+            </a>
+        </ul>
+    </div>
+</div>
+</nav>
+<!-- ini penutup navbar -->
+<div class="container">
+  <div class="row">
+    <div class="col-4">
+      <div class="card mx-auto" style="width: 18rem;">
+  <img src="assets/img/Nazhif2.jpeg" class="card-img-top" style="width: 18rem;">
+  <div class="card-body">
+    <p class="card-text">Nazhif</p>
+  </div>
+</div>
+    </div>
+    <div class="col-8">
+      <h1>selamat datang di website saya
+      </h1>
+      <p>Perkenalkan saya Nazhif Alwi Ramadhani, saya bersekolah di SMKS Jakarta Pusat 1, jurusan saya rekayasa perangkat lunak</p>
+    </div>
+  </div>
+</div>
+<!-- content 1 -->
+</div>
+
+    <!-- LINK Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
